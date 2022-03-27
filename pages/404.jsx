c8b60d9016eb696a0df0c87d72d2a03cf06f59b5
Robert Moraru <robert.moraru2012@yahoo.com>
2022-03-27 22:23:42 +0300

    added 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col w-screen h-screen justify-center content-center bg-blue">
+      <div className="h-1/2 content-bottom flex items-end justify-center">
+        <h1 className="text-9xl font-bold text-darkBlue text-center relative top-6">
+          404
+        </h1>
+      </div>
+      <div className="w-screen text-blue h-1/2 bg-darkBlue text-white flex flex-col justify-center content-center text-center p-3">
+        <h1 className="text-4xl">Această pagină este indisponibilă!</h1>
+        <h2 className="text-lg mt-3">
+          Ne pare rău, însă pagina pe care doriți să o accesați nu există, a
+          fost ștearsă ori este temporar dezactivată.
+        </h2>
+        <Link href="/">
+          <a className="p-3 bg-blue w-48 mx-auto rounded mt-5 text-darkBlue font-bold">
+            ÎNAPOI ACASĂ
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
