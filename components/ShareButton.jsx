ce5fc2b1a2ecdad48500a0071b45c80b7d341c98
Robert Moraru <robert.moraru2012@yahoo.com>
2022-04-21 10:21:27 +0300

    added share across various platforms

--- components/ShareButton.jsx
+++ components/ShareButton.jsx
@@ -11,21 +11,33 @@ import {
   Button,
 } from "@chakra-ui/react";
 
+import { ShareFill } from "react-bootstrap-icons";
+
 import {
-  Clipboard2CheckFill,
-  Clipboard2PlusFill,
-  ShareFill,
-} from "react-bootstrap-icons";
+  EmailShareButton,
+  EmailIcon,
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  FacebookIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  RedditShareButton,
+  RedditIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+} from "react-share";
 
 const ShareButton = ({ id }) => {
-  const [copied, setCopied] = useState(false);
-  const url = `${
+  const shareUrl = `${
     process.env.NEXT_PUBLIC_VERCEL_URL
       ? process.env.NEXT_PUBLIC_VERCEL_URL
       : "http://localhost:3000"
   }/post/${id}`;
   return (
-    <Popover placement="top-start" onClose={() => setCopied(false)}>
+    <Popover placement="top-start">
       <PopoverTrigger>
         <Button
           leftIcon={<ShareFill className="text-blue" />}
@@ -39,32 +51,47 @@ const ShareButton = ({ id }) => {
         <PopoverArrow />
         <PopoverCloseButton />
         <PopoverHeader>Distribuie această postare</PopoverHeader>
-        <PopoverBody>
-          {copied ? (
-            <Button
-              leftIcon={<Clipboard2CheckFill />}
-              colorScheme="yellow"
-              disabled
-              className="w-full"
-              onClick={() => {
-                navigator.clipboard.writeText(url);
-              }}
-            >
-              Copiat
-            </Button>
-          ) : (
-            <Button
-              leftIcon={<Clipboard2PlusFill />}
-              colorScheme="blue"
-              className="w-full"
-              onClick={() => {
-                navigator.clipboard.writeText(url);
-                setCopied(true);
-              }}
-            >
-              Copiază codul
-            </Button>
-          )}
+        <PopoverBody className="flex flex-row justify-evenly flex-wrap">
+          <EmailShareButton
+            url={shareUrl}
+            subject="Aruncă o privire la această postare de pe IdeeRO."
+            body="body"
+          >
+            <EmailIcon size={32} round />
+          </EmailShareButton>
+          <FacebookShareButton
+            url={shareUrl}
+            quote="Aruncă o privire la această postare de pe IdeeRO."
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+          <FacebookMessengerShareButton url={shareUrl} appId="521270401588372">
+            <FacebookMessengerIcon size={32} round />
+          </FacebookMessengerShareButton>
+          <TwitterShareButton
+            url={shareUrl}
+            title="Aruncă o privire la această postare de pe IdeeRO."
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+          <WhatsappShareButton
+            url={shareUrl}
+            title="Aruncă o privire la această postare de pe IdeeRO."
+            separator=": "
+          >
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+          <LinkedinShareButton url={shareUrl}>
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+          <RedditShareButton
+            url={shareUrl}
+            title="Aruncă o privire la această postare de pe IdeeRO."
+            windowWidth={660}
+            windowHeight={460}
+          >
+            <RedditIcon size={32} round />
+          </RedditShareButton>
         </PopoverBody>
       </PopoverContent>
     </Popover>
